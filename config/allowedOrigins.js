00af7f71dc50de092d29944d8eff341dfd632559
techiemokhele <neomokhele23@gmail.com>
2024-07-13 15:09:56 +0200

    Implement custom and 3rd party middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3500',
+    'http://127.0.0.1:3500',
+    'https://neomokhele-technotes-vercel.app',
+    'https://www.neomokhele-technotes-vercel.app',
+]
+
+module.exports = allowedOrigins
